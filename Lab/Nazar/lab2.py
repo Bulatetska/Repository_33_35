5cc50a05891bd43d6626adc8b060e303475b0ecd
bodia <bodia.naza4669@gmail.com>
2022-09-24 23:10:43 +0200

    added lab1 and lab2

---
+++ Lab/Nazar/lab2.py
@@ -0,0 +1,17 @@
+def main():
+    price = int(input("Впишіть кількість гривень:"))
+
+    print("1)")
+    if (price > 100) and (price <= 500):
+        print("Ціна зі знижкою: ", price - (price*3)/100, "Знижка:", (price*3)/100)
+    elif (price > 500) and (price <= 1000):
+        print("Ціна зі знижкою: ", price - (price * 5) / 100, "Знижка:", (price * 5) / 100)
+    elif (price > 1000):
+        print("Ціна зі знижкою: ", price - (price * 10) / 100, "Знижка:", (price * 10) / 100)
+    else:
+        print("Немає знижки")
+    print("2)")
+    sequence = str(input("Введіть будь-яке стрінг значення:"))
+    b = sequence if (len(sequence) >= 1) else None
+    print(b)
+main()
